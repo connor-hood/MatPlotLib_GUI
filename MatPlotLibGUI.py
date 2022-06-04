362c141cf37ab51cbb6998f65a521594d156bfc2
Connor Hood <connor_hood@yahoo.com>
2022-06-04 14:49:04 -0500

    Added pie function and postioned description text properly

--- MatPlotLibGUI.py
+++ MatPlotLibGUI.py
@@ -2,15 +2,26 @@ import PySimpleGUI as sg
 import matplotlib.pyplot as plt
 
 
+line_data = [0.1, 0.2, 0.5, 0.7]
+pie_data = [.37, .31, .18, .11]
+
+
 def draw_line():
     plt.title("Test with MatPlotLib in PySimpleGui")
-    plt.text(0, 0.6, "Line Graph Version")
-    plt.plot([0.1, 0.2, 0.5, 0.7])
+    plt.text(0.535, 0.703, "Line Graph Version")
+    plt.plot(line_data)
     plt.show(block=False)
 
-layout = [[sg.Button('Plot'), sg.Cancel(), sg.Button('Popup')]]
+def draw_pie():
+    plt.title("Test with MatPlotLib in PySimpleGui")
+    plt.text(-0.781, 1.077, "Pie Graph")
+    plt.pie(pie_data, autopct='%d%%')
+    plt.axis('equal')
+    plt.show()
 
-window = sg.Window('Have some Matplotlib....', layout)
+layout = [[sg.Button('Plot'), sg.Button('Pie'), sg.Cancel(), sg.Button('Popup')]]
+
+window = sg.Window('Have some Matplotlib...', layout)
 
 while True:
     event, values = window.read()
@@ -18,6 +29,8 @@ while True:
         break
     elif event == 'Plot':
         draw_line()
+    elif event == 'Pie':
+        draw_pie()
     elif event == 'Popup':
         sg.popup('Yes, your application is still running')
 window.close()
